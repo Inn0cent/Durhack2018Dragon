efc12b9747839a28b2c18d4485b95665188d4042
Inn0cent <Inn0cent@users.noreply.github.com>
2018-11-17 19:25:25 +0000

    Added State change detection

--- dragonInput.ino
+++ dragonInput.ino
@@ -1,6 +1,11 @@
 const int buttonIncr = 4;
 const int buttonDecr = 5;
 
+int pressedIncr;
+int pressedDecr;
+int lastIncr;
+int lastDecr;
+
 void setup() {
     pinMode(buttonIncr, INPUT);
     pinMode(buttonDecr, INPUT);
@@ -9,13 +14,33 @@ void setup() {
 }
 
 void loop() {
-    int pressedIncr = digitalRead(buttonIncr);
-	int pressedDecr = digitalRead(buttonDecr);
-    if (pressedIncr == 1) {
-        Serial.println("increment");
-    }
+    pressedIncr = digitalRead(buttonIncr);
+	pressedDecr = digitalRead(buttonDecr);
 	
-	if (pressedDecr == 1) {
-		Serial.println("decrement");
+	// compare the buttonState to its previous state
+	if (pressedIncr != lastIncr) {
+		// if the state has changed, increment the counter
+		if (pressedIncr == HIGH) {
+			// if the current state is HIGH then the button went from off to on:
+			Serial.println("increment");
+		}
+		// Delay a little bit to avoid bouncing
+		delay(50);
 	}
+	// save the current state as the last state, for next time through the loop
+	lastIncr = pressedIncr;
+	
+	// compare the buttonState to its previous state
+	if (pressedDecr != lastDecr) {
+		// if the state has changed, increment the counter
+		if (pressedDecr == HIGH) {
+			// if the current state is HIGH then the button went from off to on:
+			Serial.println("decrement");
+		}
+		// Delay a little bit to avoid bouncing
+		delay(50);
+	}
+	// save the current state as the last state, for next time through the loop
+	lastDecr = pressedDecr;
+	
 }
